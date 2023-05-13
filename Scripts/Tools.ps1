acb5894f0329ac14c6c3be009e6729fd0a53b10a
Adrian Gaborek <A.Gaborek@devnet.de>
2023-05-14 01:41:16 +0200

    mend

--- Scripts/Tools.ps1
+++ Scripts/Tools.ps1
@@ -15,7 +15,7 @@
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
     if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
         $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
-        Start-Process -Verb Runas -ArgumentList $CommandLine wt.exe ('"{0}"' -f (Get-Process -Id $PID).Path)
+        Start-Process -Verb -ArgumentList $CommandLine Runas wt.exe ('"{0}"' -f (Get-Process -Id $PID).Path)
         Exit
     }
 }
