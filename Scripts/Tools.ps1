f774a6b3fb748f4955bdb5f43fc916cb65385055
Adrian Gaborek <A.Gaborek@devnet.de>
2023-05-14 02:11:06 +0200

    mend

--- Scripts/Tools.ps1
+++ Scripts/Tools.ps1
@@ -14,7 +14,7 @@
     # Self-elevate the script if required
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
     if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-        $CommandLine = "-FilePath `"$($MyInvocation.MyCommand.Path)`" $MyInvocation.UnboundArguments"
+        $CommandLine = "-File `"$($MyInvocation.MyCommand.Path)`" $MyInvocation.UnboundArguments"
         Start-Process -Verb RunAs wt -ArgumentList $CommandLine
         Exit
     }
