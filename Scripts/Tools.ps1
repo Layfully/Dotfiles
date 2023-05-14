98f3c78358ecb94188740e97f60709315f7264b3
Adrian Gaborek <A.Gaborek@devnet.de>
2023-05-14 02:35:19 +0200

    mend

--- Scripts/Tools.ps1
+++ Scripts/Tools.ps1
@@ -16,7 +16,7 @@ if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
         $CommandLine = $MyInvocation.MyCommand.Path + $MyInvocation.UnboundArguments
         Write-Output $CommandLine
-        Start-Process -Verb RunAs wt -ArgumentList "C:\Users\admin\Dotfiles\Scripts\Tools.ps1"
+        Start-Process -Verb RunAs wt -ArgumentList $CommandLine
         Exit
     }
 }
