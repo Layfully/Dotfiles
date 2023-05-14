5df3809f2b7a461fe2bd856de3ca43c7e0a64a29
Adrian Gaborek <A.Gaborek@devnet.de>
2023-05-14 12:03:18 +0200

    feat(z): add alias for using fzf with z

--- Config/user_profile.ps1
+++ Config/user_profile.ps1
@@ -229,3 +229,4 @@ Set-Alias g git
 Set-Alias grep findstr
 Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+Set-Alias zz Invoke-FuzzyZLocation
