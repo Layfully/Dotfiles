af8ef41505d155f1c160d07ada7d1330ed05eea4
Adrian Gaborek <A.Gaborek@devnet.de>
2025-02-13 23:17:34 +0100

    feat(vs): add script to automatically export the visual studio install profile on commit

---
+++ Scripts/GitHooks/SaveVsInstallProfile.ps1
@@ -0,0 +1,2 @@
+#used to save a list of vs code extensions for reinstall
+&"C:\Program Files (x86)\Microsoft Visual Studio\Installer\setup.exe" export --quiet --productId "Microsoft.VisualStudio.Product.Professional" --channelId "VisualStudio.17.Release" --config "$USERPROFILE/Dotfiles/Config/VisualStudio/installationConfig2022.vsconfig"
