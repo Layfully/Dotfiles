d7e0dea3d3caf92e10d4c688b1219c44fc9dd3b3
Adrian Gaborek <git@adriangaborek.dev>
2026-07-23 14:31:36 +0200

    Remove commented-out Visual Studio settings configuration from Tools.ps1

--- Scripts/Tools.ps1
+++ Scripts/Tools.ps1
@@ -198,10 +198,6 @@ $configItems = @(
         ProfileFullPath = Join-Path -Path $env:LOCALAPPDATA -ChildPath "Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
         TargetPath      = Join-Path -Path $env:USERPROFILE -ChildPath "Dotfiles\Config\WindowsTerminal\settings.json"
     },
-    #@{
-    #    ProfileFullPath = Join-Path -Path $env:LOCALAPPDATA -ChildPath "microsoft\visualstudio\17.0_12268c9f\settings\CurrentSettings.vssettings"
-    #    TargetPath      = Join-Path -Path $env:USERPROFILE -ChildPath "Dotfiles\Config\VisualStudio\settings.vssettings"
-    #}
     @{
         ProfileFullPath = Join-Path -Path $env:LOCALAPPDATA -ChildPath "UniGetUI\Configuration"
         TargetPath      = Join-Path -Path $env:USERPROFILE -ChildPath "Dotfiles\Config\UniGetUI"
